6be29bc69f4991dec3bb2849fc9e88ed737a3c30
Eshwanth <eshwanthkartitr@gmail.com>
2024-06-02 03:36:57 +0530

    Code snapshot

--- script.js
+++ script.js
@@ -14,4 +14,13 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
-  
+  function copyCode() {
+    const codeBlock = document.getElementById('codeBlock');
+    const textArea = document.createElement('textarea');
+    textArea.value = codeBlock.innerText;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
+    alert('Code copied to clipboard');
+}
